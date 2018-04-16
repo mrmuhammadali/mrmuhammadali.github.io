9e1815eb488222b6d885b1c6969ce317b00821b8
Muhammad Ali <muhammad.ali@emumba.com>
2018-04-17 00:28:34 +0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,26 +3,17 @@ import React from "react";
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
 
 // src
+import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Portfolio from "./components/Portfolio";
 
-const theme = createMuiTheme({
-  palette: {
-    type: "dark"
-  }
-});
+const theme = createMuiTheme({ palette: { type: "dark" } });
 
 export default () => (
   <MuiThemeProvider theme={theme}>
-    <div>
-      <Header />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </div>
+    {[<Header />, <About />, <Portfolio />, <Contact />, <Footer />]}
   </MuiThemeProvider>
 );
