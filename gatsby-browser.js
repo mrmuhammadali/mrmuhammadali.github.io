b1ec0e92e150eb4678dea99cbb99cb10f35c5ad0
Muhammad Ali <mr.muhammad.ali@live.com>
2021-12-02 00:45:08 +0500

    Removes 400 font variant

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import smoothscroll from "smoothscroll-polyfill";
 import "@fontsource/titillium-web/200.css";
 import "@fontsource/titillium-web/300.css";
-import "@fontsource/titillium-web/400.css";
 
 import "./src/styles/global.css";
 
