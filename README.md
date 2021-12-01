e32c6a22cc564e2b39b7c2edfe46a408fc2afa66
Muhammad Ali <mr.muhammad.ali@live.com>
2021-12-02 00:26:31 +0500

    Removes files

--- README.md
+++
@@ -1,2 +0,0 @@
-This project is bootstrapped with [Gatsby](https://www.gatsbyjs.org/).
-It is hosted over [muhammadali.dev](https://muhammadali.dev).
--- src/components/Icons/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Amazon } from "./Amazon";
-export { default as Github } from "./Github";
-export { default as Launch } from "./Launch";
-export { default as Npm } from "./Npm";
-export { default as Skype } from "./Skype";
-export { default as Telegram } from "./Telegram";
